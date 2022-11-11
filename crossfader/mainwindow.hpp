8c9945600750766873b184854cee770eebc43600
Hoha <43900292+excitin9@users.noreply.github.com>
2022-11-11 15:07:12 +0800

    Add files via upload

---
+++ crossfader/mainwindow.hpp
@@ -0,0 +1,82 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+/*
+    Copyright (c) 2009-10 Qtrac Ltd. All rights reserved.
+
+    This program or module is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version. It is provided
+    for educational purposes and is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+    the GNU General Public License for more details.
+*/
+
+#include <QLabel>
+#include <QList>
+#include <QMap>
+#include <QMainWindow>
+#include <QPointer>
+#include <QProgressBar>
+
+
+class CrossFader;
+class StatusButtonBar;
+class QLineEdit;
+class QPushButton;
+class QScrollArea;
+class QSpinBox;
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent=0);
+
+private slots:
+    void setFirstImage();
+    void setLastImage();
+    void generateOrCancelImages();
+    void saving(const QString &filename);
+    void saved(bool saved, const QString &filename);
+    void finished();
+    void updateUi();
+    void quit();
+
+private:
+    enum StopState {Stopping, Terminating};//第一种是因为用户的取消导致的 停止，第二种是因为用户的退出而导致的停止
+
+    void createWidgets();
+    void createLayout();
+    void createConnections();
+    void setImageFile(QLabel *targetLabel, const QString &which);
+    void createAndRunACrossFader(int number, const QImage &firstImage,
+                                 const QImage &lastImage);
+    void cleanUp(StopState stopState=Stopping);
+
+    QPushButton *firstButton;
+    QLabel *firstLabel;
+    QLabel *lastLabel;
+    QLabel *baseNameLabel;
+    QLineEdit *baseNameEdit;
+    QLabel *numberLabel;
+    QSpinBox *numberSpinBox;
+    QPushButton *lastButton;
+    QPushButton *generateOrCancelButton;
+    QPushButton *quitButton;
+    StatusButtonBar *statusBar;
+    QScrollArea *scrollArea;
+    QWidget *progressWidget;
+
+//将指向进度条的那些指针保存在QMap中，每一个键都是进度条显式图像处理进度的图像文件名
+
+    QMap<QString, QPointer<QProgressBar> > progressBarForFilename;
+    QList<QPointer<QLabel> > progressLabels;
+    QList<QPointer<CrossFader> > crossFaders;
+    bool canceled;
+};
+
+#endif // MAINWINDOW_HPP
